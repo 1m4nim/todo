4298934ece04e39bc8fa705c9d7ee12a1d82c986
1m4nim <109068169+1m4nim@users.noreply.github.com>
2024-06-01 15:08:09 +0900

    送信したものを書きだしたい

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
-xrh.onreadystatechange = function () {
-    if (xrh.readyState === 4 && xrh.status === 200) {
+document.getElementById('myForm').addEventListener('submit', function (event) {
 
-    }
-    
-}
+    // 入力された値を取得
+    const confirmValue = document.getElementById('confirmInput').value;
+
+    // 取得した値を表示
+    document.getElementById('confirmOutput').innerText = confirmValue;
+});
 
 
