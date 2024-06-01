f7e4562c9bae1e902ccca2034e067e771e34366b
1m4nim <109068169+1m4nim@users.noreply.github.com>
2024-06-01 20:23:55 +0900

    完了できるようにした

--- script.js
+++ script.js
@@ -1,13 +1,32 @@
 const btn = document.getElementById('todo_btn');
 const ul = document.getElementById('todo_ul');
 
+//list-delete
+function addDeleteButton(li) {
+    const deleteButton = document.createElement("button");
+    deleteButton.textContent = "完了！";
+    deleteButton.classList.add("delete-btn");
+    deleteButton.addEventListener("click", function () {
+        li.remove();
+    });
+    li.appendChild(deleteButton);
+}
+
+
+
+
 btn.addEventListener('click', function () {
     let text = document.getElementById('input_todo').value;
 
 
     if (text.trim() !== '') {
-        ul.insertAdjacentHTML('beforeend', '<li>' + text + '</li>');
-        clearText(); // テキストボックスをクリアする
+        const li = document.createElement("li");
+        li.textContent = text;
+        addDeleteButton(li);
+        ul.appendChild(li);
+        clearText();
+        //ul.insertAdjacentHTML('beforeend', '<li>' + text + '</li>');
+        //clearText(); // テキストボックスをクリアする
     } else {
         alert('テキストボックスが空です。');
     }
@@ -17,3 +36,4 @@ function clearText() {
     var textForm = document.getElementById("input_todo");
     textForm.value = "";
 }
+
