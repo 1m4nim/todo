e7d1ea0c8e576c1f02b1e552ade23164f9aca0b9
1m4nim <109068169+1m4nim@users.noreply.github.com>
2024-06-01 15:19:00 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,10 +1,9 @@
-document.getElementById('myForm').addEventListener('submit', function (event) {
-
+document.getElementById('myForm').addEventListener('submit', function(event) {
+    event.preventDefault(); // フォームのデフォルトの送信動作をキャンセル
+            
     // 入力された値を取得
     const confirmValue = document.getElementById('confirmInput').value;
 
     // 取得した値を表示
     document.getElementById('confirmOutput').innerText = confirmValue;
 });
-
-
